50eade06a7e3bcf40c8360e2d5467d0b62203b96
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2023-11-10 00:03:09 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -465,7 +465,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://github.com/MrMKN/PROFESSOR-BOT')
+            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='t.me/wmteams')
             ],[
             InlineKeyboardButton('‹ Bᴀᴄᴋ', 'about')
         ]]
