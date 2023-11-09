97fa973b9d4ed39e6514315cfbd5e8cb0fe032f4
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2023-11-10 00:11:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
 PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
-LOG_MSG = "{}  Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨ @vis_hnu_bot\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
+LOG_MSG = "{}  Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨ @vis_hnu_bot\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n©️ Cᴏᴩʏʀɪɢʜᴛ: THIS IS VISHNU A   !"
 
 
 
